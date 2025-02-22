c264a6217e2506e5e4165d047fc3f6b8ec1e62b2
Allan J. Souza <allanjsouza@gmail.com>
2025-02-21 21:56:52 -0300

    feat: change to esbuild and also include notifications

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import "controllers"
+import "./controllers"
 
 import "trix"
 import "@rails/actiontext"
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,5 @@
 class Course < ApplicationRecord
   has_rich_text :description
-
-  validates_presence_of :title, :description
-
   belongs_to :user
+  validates_presence_of :title, :description
 end
